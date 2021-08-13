6e8748bec9131d3fc02019954b9d612473da1e33
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-08-13 12:36:56 -0700

    Added animation when opeining / closingnav bar

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ function toggleNav() {
   navTriggerEl.addEventListener('click', function () {
     navEl.classList.toggle('open')
     contentEl.classList.toggle('shift')
-    animateHamburgers()
+    animateHamburger()
   })
 }
 
-function animateHamburgers() {
-  for (let i = 0; i < hamburgerBarsEl.length; i++) {
-    hamburgerBarsEl[i].classList.add('change')
+function animateHamburger() {
+  for (let item of hamburgerBarsEl) {
+    item.classList.toggle('change')
   }
 }
 
