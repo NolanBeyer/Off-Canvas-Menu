2661ccec8705e5128663f906bb816800eaa3ba99
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-08-15 01:05:30 -0700

    toggle to open / close menu bar

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const navTriggerEl = document.querySelector('.hamburger')
 const navEl = document.querySelector('.nav')
 const contentEl = document.querySelector('.content')
-const hamburgerBarsEl = document.getElementsByTagName('span')
+const hamburgerBarsEl = document.querySelectorAll('span')
 
 function toggleNav() {
   navTriggerEl.addEventListener('click', function () {
     navEl.classList.toggle('open')
     contentEl.classList.toggle('shift')
-    animateHamburger()
+    animateHamburgers()
   })
 }
 
-function animateHamburger() {
+function animateHamburgers() {
   for (let item of hamburgerBarsEl) {
     item.classList.toggle('change')
   }
